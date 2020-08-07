6f703279acd2814e881be1c1e1e8486f09b23a0b
Albert Kim <alkim@alkim.org>
2020-08-07 11:10:40 -0400

    Ready for alpha release

--- mbsync_watcher/main.py
+++ mbsync_watcher/main.py
@@ -4,37 +4,40 @@ import argparse
 import asyncio
 import logging
 import os
+import signal
 import ssl
 import sys
+import shlex
 import time
 
 from aioimaplib import aioimaplib
 
-from .parse_config import Config
+from .parse_mbsyncrc import Mbsyncrc
+from .config import Config
 
 
-CONFIG_PATH = os.path.expanduser('~/.mbsyncrc')
+MBSYNCRC_PATH = os.path.expanduser("~/.mbsyncrc")
 
-CHECK_PERIOD = 5 * 60  # in seconds
+LOGGER_NAMESPACE = "mbsync-watcher"
 
-LOGGER_NAMESPACE = 'mbsync-watcher'
-
+config = Config()
 
 # Monkey patching aioimaplib to support starttls
 async def protocol_starttls(self, host, ssl_context=None):
-    if 'STARTTLS' not in self.capabilities:
-        aioimaplib.Abort('server does not have STARTTLS capability')
-    if hasattr(self, '_tls_established') and self._tls_established:
-        aioimaplib.Abort('TLS session already established')
+    if "STARTTLS" not in self.capabilities:
+        aioimaplib.Abort("server does not have STARTTLS capability")
+    if hasattr(self, "_tls_established") and self._tls_established:
+        aioimaplib.Abort("TLS session already established")
 
-    response = await self.execute(aioimaplib.Command(
-        'STARTTLS', self.new_tag(), loop=self.loop))
-    if response.result != 'OK':
+    response = await self.execute(
+        aioimaplib.Command("STARTTLS", self.new_tag(), loop=self.loop)
+    )
+    if response.result != "OK":
         return response
 
     if ssl_context is None:
         ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    sock = self.transport.get_extra_info('socket')
+    sock = self.transport.get_extra_info("socket")
     sock.setblocking(True)
     sock = ssl_context.wrap_socket(sock, server_hostname=host)
     # XXX: This is kind of a hack. It works since the transport interface
@@ -49,41 +52,40 @@ async def protocol_starttls(self, host, ssl_context=None):
     return response
 
 
-
 async def imap_starttls(self):
-    return (await asyncio.wait_for(
-        self.protocol.starttls(self.host), self.timeout))
+    return await asyncio.wait_for(self.protocol.starttls(self.host), self.timeout)
+
 
 aioimaplib.IMAP4ClientProtocol.starttls = protocol_starttls
 aioimaplib.IMAP4.starttls = imap_starttls
 
 
-async def watch(config, channel, queue):
-    store = config.stores[config.channels[channel]['master']]
-    account = config.accounts[store['account']]
+async def watch(mbsyncrc, channel, queue):
+    store = mbsyncrc.stores[mbsyncrc.channels[channel]["master"]]
+    account = mbsyncrc.accounts[store["account"]]
 
     while True:
-        if account['ssl']:
-            imap_client = aioimaplib.IMAP4_SSL(host=account['host'])
+        if account["ssl"]:
+            imap_client = aioimaplib.IMAP4_SSL(host=account["host"])
         else:
-            imap_client = aioimaplib.IMAP4(host=account['host'])
+            imap_client = aioimaplib.IMAP4(host=account["host"])
 
         await imap_client.wait_hello_from_server()
-        assert('IDLE' in imap_client.protocol.capabilities)
+        assert "IDLE" in imap_client.protocol.capabilities
 
-        if not account['ssl']:
+        if not account["ssl"]:
             await imap_client.starttls()
-            #protocol = imap_client.protocol
-            #await protocol.execute(
+            # protocol = imap_client.protocol
+            # await protocol.execute(
             #    aioimaplib.Command('STARTTLS', protocol.new_tag()))
 
-        await imap_client.login(account['user'], account['password'])
+        await imap_client.login(account["user"], account["password"])
         await imap_client.select()
 
         asyncio.ensure_future(imap_client.idle())
         while True:
             msg = await imap_client.wait_server_push()
-            if 'EXISTS' in msg:
+            if "EXISTS" in msg:
                 queue.put_nowait(time.time())
 
 
@@ -97,19 +99,46 @@ async def sync(channel, queue):
                 break
 
         proc = await asyncio.create_subprocess_exec(
-            'mbsync', channel,
+            "mbsync",
+            channel,
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
+            stderr=asyncio.subprocess.PIPE,
+        )
         stdout, stderr = await proc.communicate()
 
         if proc.returncode != 0:
-            print('ERROR: {}'.format(channel))
-            print('== stdout ==')
+            print("ERROR: {}".format(channel))
+            print("== stdout ==")
             print(stdout)
-            print('== stderr ==')
+            print("== stderr ==")
             print(stderr)
         else:
-            logger.info('synced {}'.format(channel))
+            logger.info("synced {}".format(channel))
+
+        for hook in config.post_sync_hooks():
+            proc = await asyncio.create_subprocess_exec(
+                *shlex.split(hook),
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            stdout, stderr = await proc.communicate()
+            if proc.returncode != 0:
+                print("ERROR: [hook {}] {}".format(hook, channel))
+                print("== stdout ==")
+                print(stdout)
+                print("== stderr ==")
+                print(stderr)
+            else:
+                # print("Ran {}".format(hook))
+                pass
+                # proc = await asyncio.create_subprocess_exec(
+                #    'pgrep', 'alot',
+                #    stdout=asyncio.subprocess.PIPE,
+                #    stderr=asyncio.subprocess.PIPE)
+                # stdout, stderr = await proc.communicate()
+                # for line in stdout.encode().split('\n'):
+                #    pid = int(line.strip())
+                #    os.kill(pid, signal.SIGUSR1)
 
 
 async def timer(queues):
@@ -117,39 +146,41 @@ async def timer(queues):
         for queue in queues:
             queue.put_nowait(time.time())
 
-        await asyncio.sleep(CHECK_PERIOD)
+        await asyncio.sleep(config.check_interval())
 
 
 def main():
     logger = logging.getLogger(LOGGER_NAMESPACE)
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter(
-        '[{}] %(asctime)s %(message)s'.format(LOGGER_NAMESPACE)))
+    handler.setFormatter(
+        logging.Formatter("[{}] %(asctime)s %(message)s".format(LOGGER_NAMESPACE))
+    )
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
     parser = argparse.ArgumentParser(
-        description='Watch mailboxes using IDLE and sync with mbsync.')
-    parser.add_argument('names', metavar='name', nargs='+',
-                        help='names of channels or groups to sync')
+        description="Watch mailboxes using IDLE and sync with mbsync."
+    )
+    parser.add_argument(
+        "names", metavar="name", nargs="+", help="names of channels or groups to sync"
+    )
 
     args = parser.parse_args()
-    config = Config(CONFIG_PATH)
+    mbsyncrc = Mbsyncrc(MBSYNCRC_PATH)
 
     channels = []
     for name in args.names:
-        if name in config.groups:
-            channels.extend(config.groups[name]['channels'])
-        elif name in config.channels:
+        if name in mbsyncrc.groups:
+            channels.extend(mbsyncrc.groups[name]["channels"])
+        elif name in mbsyncrc.channels:
             channels.append(name)
         else:
-            raise Exception('Could not find mention of {} in config'.format(
-                name))
+            raise Exception("Could not find mention of {} in mbsyncrc".format(name))
 
     loop = asyncio.get_event_loop()
     queues = []
     for channel in channels:
         queue = asyncio.Queue()
-        loop.create_task(watch(config, channel, queue))
+        loop.create_task(watch(mbsyncrc, channel, queue))
         loop.create_task(sync(channel, queue))
         queues.append(queue)
     loop.create_task(timer(queues))
@@ -157,5 +188,5 @@ def main():
     loop.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
 
-setup(name='mbsync-watcher',
-      version='0.1.0',
-      description='Watch mailboxes using IDLE and sync with mbsync.',
-      author='Albert Kim',
-      author_email='alkim@alkim.org'
-      )
+from setuptools import setup
+
+setup(
+    name="mbsync-watcher",
+    version="0.1.0",
+    packages=["mbsync_watcher"],
+    description="Watch mailboxes using IDLE and sync with mbsync.",
+    author="Albert Kim",
+    author_email="alkim@alkim.org",
+    install_requires=["pyyaml", "aioimaplib"],
+    scripts=["bin/mbsync_watcher"],
+    data_files=[
+        (os.path.expanduser("~/.config/mbsync_watcher"), ["config.yaml"]),
+        (os.path.expanduser("~/.config/systemd/user"), ["mbsync-watcher.service"]),
+    ],
+)
